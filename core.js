df9965d5a853db8d6f7c0b4195c0d0bd4b05e91f
Zeroundefined <colaZ_1667@163.com>
2018-07-07 19:04:52 +0800

    update service

--- core.js
+++ core.js
@@ -630,12 +630,12 @@ app.post('/screenshot', async (req, res) => {
   console.log(page);
   page.setViewport({
     width,
-    height: height - 60
+    height
   })
   await page.goto('http://www.baidu.com');
   await page.evaluate(`
+      document.body.innerHTML = \`${content}\`;
       document.querySelectorAll('.highcharts-credits').forEach(item => item.style.display = 'none');
-      document.body.innerHTML = \`${content}\`
     `);
   await page.screenshot({ path: 'report.png' });
   await browser.close();
